93d61f271bca0e343b891788aa9315c1411703c7
wanghao1993 <whao53333@gmail.com>
2023-08-08 23:54:39 +0800

    feat: register

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  register(@Body() resisterUser: RegisterUserDto) {
+    this.userService.register(resisterUser);
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,4 +1,55 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { RegisterUserDto } from './dto/user.dto';
+import { RedisService } from 'src/redis/redis.service';
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject(RedisService)
+  private redisService: RedisService;
+  private logger = new Logger();
+  private userRepository: Repository<User>;
+
+  async register(user: RegisterUserDto) {
+    const captcha = await this.redisService.get(`captch_${user.email}`);
+
+    if (!captcha) {
+      throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
+    }
+
+    if (user.captcha !== captcha) {
+      throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
+    }
+
+    const foundUser = await this.userRepository.findOneBy({
+      username: user.username,
+    });
+
+    if (foundUser) {
+      throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST);
+    } else {
+      const newUser = new User();
+      newUser.username = user.username;
+      newUser.password = user.password;
+      newUser.email = user.email;
+      newUser.nickName = user.nickName;
+
+      try {
+        await this.userRepository.save(newUser);
+        return {
+          code: 200,
+          message: '注册成功',
+        };
+      } catch (e) {
+        this.logger.error(e, UserService);
+        return '注册失败';
+      }
+    }
+  }
+}
