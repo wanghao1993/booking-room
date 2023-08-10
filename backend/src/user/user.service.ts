eeaea39fe234729ef74801e8ada7a2d1b1643aeb
wanghao1993 <whao53333@gmail.com>
2023-08-10 23:57:14 +0800

    feat: 验证码

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -21,7 +21,7 @@ export class UserService {
   private userRepository: Repository<User>;
 
   async register(user: RegisterUserDto) {
-    const captcha = await this.redisService.get(`captch_${user.email}`);
+    const captcha = await this.redisService.get(`captcha_${user.email}`);
 
     if (!captcha) {
       throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
